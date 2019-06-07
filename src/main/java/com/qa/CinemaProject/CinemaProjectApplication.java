1b0d0594b43092042293a6519264608ea8e90982
Kiregor <kiregorekal@gmail.com>
2019-06-07 11:10:57 +0100

    first commit

---
+++ src/main/java/com/qa/CinemaProject/CinemaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.qa.CinemaProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaProjectApplication.class, args);
+	}
+
+}
