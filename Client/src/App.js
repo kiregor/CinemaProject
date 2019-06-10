2cf9e2d8f60cc93f43caade2964aa64f039a1d75
HamzzAR <hamza_razeq@hotmail.co.uk>
2019-06-10 14:26:40 +0100

    Testing the stripe api

--- Client/src/App.js
+++ Client/src/App.js
@@ -2,15 +2,22 @@ import React from 'react';
 import './App.css';
 import AppNavbar from './components/AppNavbar';
 import Carousel from './components/AppCarousel';
-//import AppLogin from './components/AppLogin';
-//import Breadcrumbs from './components/AppBreadcrumbs';
+import { link } from 'fs';
 
 function App() {
   return ( 
     <div className = "App" >
       <AppNavbar/>
       <Carousel/>
+      <button onClick='goPaymentPage()'>fdfj</button>
     </div>
   );
 }
+
+function goPaymentPage(){
+  return(
+    <link to="/payment/paymentPage"></link>
+  )
+}
+
 export default App;
