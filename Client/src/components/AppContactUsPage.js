c7f6629b79261f627833a2d57fd45a34a1ffd2fe
phtb233 <stephen_621@msn.com>
2019-06-10 12:09:02 +0100

    stubbed out contact page

--- Client/src/components/AppContactUsPage.js
+++ Client/src/components/AppContactUsPage.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
+import { Form, Row, FormGroup, Label } from 'reactstrap';
+import './AppContactUsPage.css';
 
 class AppContactUsPage extends Component {
     render() {
         return (
             <div className='AppContactUsPage'>
-               <div className='container'>
-                   <h1>Contact Us</h1>
-               </div>
+                <div className='container' id='main'>
+                    <div className='row'>
+                        <div className='col-12'>
+                            <h1>Contact Us</h1>
+                        </div>
+                    </div>
+                    <div className='row'>
+                        <div className='col-12 instructions'>
+                            <p>If there are any suggestions you'd like to offer to QA Cinema, please contact us using our email form:</p>
+                        </div>
+                    </div>
+                    <div className='row'>
+                        <div className='col-12'>
+                            <Form>
+                                <FormGroup row>
+                                    <Label for='email'></Label>
+                                </FormGroup>
+                            </Form>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
