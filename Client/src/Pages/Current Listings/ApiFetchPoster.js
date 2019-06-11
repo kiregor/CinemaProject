6d69909ea880294d9e46c63529251d834a106249
Rameez <rameezjhaveri94@gmail.com>
2019-06-11 15:54:21 +0100

    hidden api keys in config file.

--- Client/src/Pages/Current Listings/ApiFetchPoster.js
+++ Client/src/Pages/Current Listings/ApiFetchPoster.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import { Spinner } from 'reactstrap';
 import MoviePoster from './ApiMoviePoster';
+import ApiKey from '../../config.config';
+
 
 //PATH FOR MOVE POSTER: http://tmdb.org/t/p/<SIZE>/<POSTER_PATH>;
 //SIZE: W92, W154, W185, W342, W500, W700;
@@ -9,15 +11,22 @@ class ApiFetchPoster extends Component{
 
     constructor(props){
         super(props);
+
+    {/*}    var today = today.getFullYear() + '-' + today.getMonth() + '-' + today.getDate();
+        console.log(today); 
+    */}
         this.state= {
+            timestamp: new Date().getTime(),
             movies: [],
             isLoaded: false,
         }
 
-    this.APIKEY=process.env.REACT_APP_APIKEY
+    this.Api=ApiKey
     }
 
     render() {
+        
+        var {timestamp} = this.state
         var { isLoaded} = this.state;
         if (!isLoaded) {
             return <div><Spinner/></div>
@@ -26,6 +35,7 @@ class ApiFetchPoster extends Component{
             return(
                 <div className="ApiFetchPoster">
                     <MoviePoster movies={this.state.movies}/>
+                    
                 </div>
                 
             );
@@ -35,7 +45,7 @@ class ApiFetchPoster extends Component{
     //Change upcoming to 'now_playing' for movies currently playing 3e9a89831a2e61d47f06983917822671
 
     componentDidMount = (e) => {
-        fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=3e9a89831a2e61d47f06983917822671&language=en-US&page=1`)
+        fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${this.state.Api}&language=en-US&page=1`)
         .then(data => data.json())
         .then(data => {
             console.log(data);
@@ -45,8 +55,5 @@ class ApiFetchPoster extends Component{
             })
         })
     }
-
 }
-
-
 export default ApiFetchPoster;
