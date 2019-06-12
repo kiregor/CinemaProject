cf3d223b96e091a93fc660faa5a831b52eca70e9
Rameez <rameezjhaveri94@gmail.com>
2019-06-12 18:42:47 +0100

    Added api call to fetch for current movie details

--- Client/src/components/Current Listings/ApiFetchPoster.js
+++ Client/src/components/Current Listings/ApiFetchPoster.js
@@ -3,44 +3,38 @@ import { Spinner } from 'reactstrap';
 import MoviePoster from './ApiMoviePoster';
 import ApiKey from '../../config.config';
 
-
 //PATH FOR MOVE POSTER: http://tmdb.org/t/p/<SIZE>/<POSTER_PATH>;
 //SIZE: W92, W154, W185, W342, W500, W700;
 
 class ApiFetchPoster extends Component{
-
     constructor(props){
         super(props);
+<<<<<<< HEAD
 
     /* var today = today.getFullYear() + '-' + today.getMonth() + '-' + today.getDate();
         console.log(today); 
     */
+=======
+>>>>>>> Added api call to fetch for current movie details
         this.state= {
-            timestamp: new Date().getTime(),
             movies: [],
             isLoaded: false,
         }
-
     this.Api=ApiKey
     }
-
     render() {
-        
         var { isLoaded} = this.state;
         if (!isLoaded) {
-            return <div><Spinner/></div>
+            return <div ><Spinner/></div>
         }
         else {
             return(
                 <div className="ApiFetchPoster">
                     <MoviePoster movies={this.state.movies}/>
-                    
                 </div>
-                
             );
         }
     }
-// fix api config
     componentDidMount = (e) => {
         fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=3e9a89831a2e61d47f06983917822671&language=en-US&page=1`)
         .then(data => data.json())
@@ -50,6 +44,7 @@ class ApiFetchPoster extends Component{
                 movies: [...data.results],
                 isLoaded: true
             })
+
         })
     }
 }
--- Client/src/components/MoviePage/MoviePage.js
+++ Client/src/components/MoviePage/MoviePage.js
@@ -1,13 +1,47 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import { Spinner } from 'reactstrap';
+import MovieId from './ApiMovieID';
+import ApiKey from '../../config.config';
 
-class MoviePage extends Component {
+//{this.props.match.params.movietitle}
+//PATH FOR MOVE POSTER: http://tmdb.org/t/p/<SIZE>/<POSTER_PATH>;
+//SIZE: W92, W154, W185, W342, W500, W700;
+
+class ApiFetchPoster extends Component{
+    constructor(props){
+        super(props);
+        this.state= {
+            movies: [],
+            isLoaded: false,
+        }
+    this.Api=ApiKey
+    }
     render() {
-        return (
-            <div className='MoviePage'>
-                <p>{this.props.match.params.movietitle}</p>
-            </div>
-        )
+        var { isLoaded} = this.state;
+        if (!isLoaded) {
+            return <div ><Spinner/></div>
+        }
+        else {
+            return(
+                <div className="ApiFetchPoster">
+                    <MovieId movies={this.state.movies}/>
+                </div>
+            );
+        }
+    }
+
+    /* HIDE API KEY + RETREIVE SEARCH QUERY FROM URL*/
+    componentDidMount = (e) => {
+        fetch(`https://api.themoviedb.org/3/search/movie?api_key=3e9a89831a2e61d47f06983917822671&language=en-US&query=dark%20phoenix&page=1&include_adult=false`)
+        .then(data => data.json())
+        .then(data => {
+            console.log(data);
+            this.setState({ 
+                movies: [...data.results],
+                isLoaded: true
+            })
+
+        })
     }
 }
-
-export default MoviePage;
+export default ApiFetchPoster;
