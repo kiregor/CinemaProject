265a87e69e756618405869f4b69108bb7e02c7ec
LindaAllyket <LindaAllyket@academytrainee.com>
2019-06-13 10:56:29 +0100

    Page matched with Rameez'

--- Client/src/components/FutureMoviePage/ApiMoviePoster.js
+++ Client/src/components/FutureMoviePage/ApiMoviePoster.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import Movie from './ApiMovie';
+import {Container, Row, Col} from 'reactstrap';
 
 const MoviePoster = (props) => {
+   
+    console.log(props.movies[0][0].poster_path)
     return(
-        <div className="container">
-            <div className="row">
-
-               
-
+        <Container>
+            <Row>
+                <Col>
                 <div className="col s12">
                     <i className="col s12 right"></i>
-                    <Movie image={props.movies[0][0].poster_path} title={props.movies[0][0].title} />
-                    
-                    <p>{props.movies[0][0].overview} </p> 
-                     
-
+                    <img src={`http://image.tmdb.org/t/p/w500${props.movies[0][0].poster_path}`}/>
+                </div>
+                </Col> 
+                <Col>
+                    <div>
+                    {props.movies[0][0].title}
+                    </div>
+                    <div>
+                     <p>{props.movies[0][0].overview} </p> 
+                    </div>
                     <div> 
                         <p><h4>Release Date: {props.movies[0][0].release_date}</h4> </p>    
                     </div>
@@ -31,11 +37,9 @@ const MoviePoster = (props) => {
                      <div> 
                         <p>Director </p>    
                     </div>
-
-                </div>
-
-             </div>
-        </div>
+                </Col>
+             </Row>
+        </Container>
     )
 }
 
