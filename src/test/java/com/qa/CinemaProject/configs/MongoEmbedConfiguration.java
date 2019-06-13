b72962aca4f062835a88433920a507041d2cf3a8
phtb233 <stephen_621@msn.com>
2019-06-13 22:03:52 +0100

    trying to perform integration tests using embeddedmongodb

---
+++ src/test/java/com/qa/CinemaProject/configs/MongoEmbedConfiguration.java
@@ -0,0 +1,29 @@
+package com.qa.CinemaProject.configs;
+
+import java.io.IOException;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import com.mongodb.MongoClient;
+
+import cz.jirutka.spring.embedmongo.EmbeddedMongoFactoryBean;
+
+@Profile("test")
+@Configuration
+public class MongoEmbedConfiguration {
+	
+	private final String MONGO_DB_URL = "localhost";
+	private final String MONGO_DB_NAME = "embedded_db";
+	@Bean
+	public MongoTemplate mongoTemplate() throws IOException {
+		EmbeddedMongoFactoryBean mongo = new EmbeddedMongoFactoryBean();
+		mongo.setBindIp(MONGO_DB_URL);
+		MongoClient mongoClient = mongo.getObject();
+		MongoTemplate mongoTemplate = new MongoTemplate(mongoClient, MONGO_DB_NAME);
+		return mongoTemplate;
+	}
+
+}
