73e68ef548fa6e9261697c703bef6daa953f2066
HamzzAR <hamza_razeq@hotmail.co.uk>
2019-06-14 17:26:36 +0100

    Card details validation implemented, shows a pop-up message when card details are not entered

--- Client/src/components/Payment/AppCheckoutForm.js
+++ Client/src/components/Payment/AppCheckoutForm.js
@@ -2,13 +2,13 @@ import React, {Component} from 'react';
 import {CardElement, injectStripe} from 'react-stripe-elements';
 import stripeService from '../../services/stripeService';
 import { Button } from 'reactstrap';
-import { Toast, ToastBody, ToastHeader } from 'reactstrap';
+import { Toast, ToastBody, ToastHeader, Alert } from 'reactstrap';
 
 class CheckoutForm extends Component {
   seatInfo = this.props.seatInfo;
   constructor(props) {
     super(props);
-    this.state = {complete: false, show: false};
+    this.state = {show: false, visible: false};
     this.submit = this.submit.bind(this);
     this.toggle = this.toggle.bind(this);
   }
@@ -21,39 +21,47 @@ class CheckoutForm extends Component {
 
   async submit(ev) {
     let {token} = await this.props.stripe.createToken({name: "Name"});
-    this.seatInfo.token = token.id;
-    console.log(this.seatInfo);
-    stripeService.sendStripe(
-     this.seatInfo
-    ).then(
-      response => {
-        let isSuccess;
-        stripeService.getSuccessStatus().then(
-          response => {
-            isSuccess = response.data;
-            if(isSuccess == "success"){
-              this.state = {complete: false};
-              console.log("Success");
-              window.location.assign("../summary/BookingSuccessPage");
-            }else{
-              console.log("Failed");
-              this.setState({show: true});
+    if(token != undefined){
+      this.seatInfo.token = token.id;
+      console.log(this.seatInfo);
+      stripeService.sendStripe(
+       this.seatInfo
+      ).then(
+        response => {
+          let isSuccess;
+          stripeService.getSuccessStatus().then(
+            response => {
+              isSuccess = response.data;
+              if(isSuccess == "success"){
+                this.state = {complete: false};
+                console.log("Success");
+                window.location.assign("../summary/BookingSuccessPage");
+              }else{
+                console.log("Failed");
+                this.setState({show: true});
+              }
             }
-          }
-        ).catch(
-          error => {
-            console.log(error);
-          }  
-        )
-      })
+          ).catch(
+            error => {
+              console.log(error);
+            }  
+          )
+        })
+    }else{
+      this.setState({visible: true})
+      setTimeout(() => { this.setState({visible: false})}, 3000);
+    }
   }
   render() {
       return (
         <>
           {(true) &&
               <div className="checkout">
-                <CardElement />
+                <CardElement require/>
                 <br/><br/>
+                <Alert color="danger" isOpen={this.state.visible}>
+                   Please enter card details!
+                </Alert>
                 <Toast isOpen={this.state.show}>
                   <ToastHeader toggle={this.toggle} icon="danger">
                     Unsuccessful Transaction
@@ -62,7 +70,7 @@ class CheckoutForm extends Component {
                     You don't have enough money in your account for this Transaction!
                   </ToastBody>
                 </Toast>
-                <br/><br/>
+                <br/>
                 <Button color="primary" size="lg" block onClick={this.submit}>Send</Button>
               </div>
           }
