0327752ac9835ec8c8735e82699efba053c14f96
Rameez <rameezjhaveri94@gmail.com>
2019-06-14 12:41:11 +0100

    .

--- Client/src/components/HomePage/AppHomePage.js
+++ Client/src/components/HomePage/AppHomePage.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component } from 'react';
 import Carousel from './AppCarousel';
 //import FetchMovieId from './FetchMovieId';
@@ -16,23 +15,4 @@ class AppHomePage extends Component {
     }
 }
 
-=======
-import React, { Component } from 'react';
-//import Carousel from './AppCarousel';
-import FetchMovieId from './FetchMovieId';
-
-class AppHomePage extends Component {
-
-    render() {
-        return (
-            <>
-            <div className="AppHomePage" >
-                <FetchMovieId/>
-            </div>
-            </>
-        );
-    }
-}
-
->>>>>>> 7e499fc72864dce7954e504041721709881b54be
 export default AppHomePage;
