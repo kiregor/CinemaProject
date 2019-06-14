e6e54229e1fd11100b6418055c0c8f9b9efd126c
Fabian Lewis <frobian21@yahoo.co.uk>
2019-06-14 12:01:48 +0100

    added button focus colours to navbar

--- Client/src/components/Current Listings/AppPagination.js
+++ Client/src/components/Current Listings/AppPagination.js
@@ -5,7 +5,7 @@ const AppPagination = (props) => {
     const pageLinks = []
 
     for(let i=1; i <= props.pages + 1; i++){
-        let active = props.currentPage == i ? 'active' : ''
+        let active = props.currentPage === i ? 'active' : ''
 
         pageLinks.push(<li className={`${active}`} key={i} onClick={() => props.nextPage(i)}><a href="#">{i}</a></li>)
     }
