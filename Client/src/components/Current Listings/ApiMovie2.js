8e4920533f379e9d81bdd069886c9f0a60c77e5b
Rameez <rameezjhaveri94@gmail.com>
2019-06-16 21:52:37 +0100

    Added buttons to current listings page to go directly to booking or more info

--- Client/src/components/Current Listings/ApiMovie2.js
+++ Client/src/components/Current Listings/ApiMovie2.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import './poster.css'
 import {FlippingCard, FlippingCardBack, FlippingCardFront} from 'react-ui-cards';
+import {Button} from 'reactstrap';
+
 
 const Movie = (props) => {
     return (
         <div className="col s6 m4 l4">
-            <FlippingCard>
-                <FlippingCardBack>
-                    <p><h3>{props.title}</h3></p>
-                    <p><h6>{props.overview}</h6></p>
-                </FlippingCardBack>
-                <FlippingCardFront>
-                    <img src={`http://image.tmdb.org/t/p/w500${props.image}`} style={{width:"100%", height: "100%"}}/>
-                </FlippingCardFront>
-            </FlippingCard>
+            <div>
+                <FlippingCard>
+                    <FlippingCardBack>
+                        <p><h3>{props.title}</h3></p>
+                        <p style={{fontSize:12}}><h6>{props.overview}</h6></p>
+                    </FlippingCardBack>
+                    <FlippingCardFront>
+                        <img src={`http://image.tmdb.org/t/p/w500${props.image}`} style={{width:"100%", height: "100%"}}/>
+                    </FlippingCardFront>
+                </FlippingCard>
+            </div>
+            <div>
+                <span>
+                    <Button color='info'>Click Me</Button>{' '}
+                    <Button color='success'>Click Me</Button>    
+                </span>
+            </div>
         </div>
     )   
 }
