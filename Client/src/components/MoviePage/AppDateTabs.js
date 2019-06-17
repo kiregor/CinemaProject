6a76562dd2e44110b5b742d2f1ebe673c2aa7e59
LindaAllyket <LindaAllyket@academytrainee.com>
2019-06-17 18:04:49 +0100

    colour theme implemented

--- Client/src/components/MoviePage/AppDateTabs.js
+++ Client/src/components/MoviePage/AppDateTabs.js
@@ -2,6 +2,13 @@ import React from 'react';
 import {TabContent, TabPane, Nav, NavItem, NavLink, Row,Col} from 'reactstrap';
 import classnames from 'classnames';
 
+
+var bgColors = { "Mist": "#90afc5",
+                    "Stone": "#336b87",
+                    "Shadow": "#2a3132",
+                    "Autumn": "#763626",
+};
+                
 export default class AppDateTabs extends React.Component {
   constructor(props) {
     super(props);
