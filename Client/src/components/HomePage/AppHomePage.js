4a9858a8f6b38207457e9b1178a5f33508b168f4
Rameez <rameezjhaveri94@gmail.com>
2019-06-17 17:43:56 +0100

    Removed small carousel

--- Client/src/components/HomePage/AppHomePage.js
+++ Client/src/components/HomePage/AppHomePage.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Carousel from './AppCarousel';
-import AppSmallCarousel from './AppSmallCarousel';
-//import FetchMovieId from './FetchMovieId';
+
 
 class AppHomePage extends Component {
 
@@ -10,9 +9,6 @@ class AppHomePage extends Component {
             <>
             <div className="AppHomePage" >
                 <Carousel/>
-                <AppSmallCarousel/>
-                {/*
-                */}
             </div>
             </>
         );
