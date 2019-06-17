26f4d39d03e3767c40d637ce538164e95114fe09
Rameez <rameezjhaveri94@gmail.com>
2019-06-17 19:32:41 +0100

    moved buttons inline

--- Client/src/components/Current Listings/ApiMovie2.js
+++ Client/src/components/Current Listings/ApiMovie2.js
@@ -6,23 +6,26 @@ import {Button} from 'reactstrap';
 
 const Movie = (props) => {
     return (
-        <div className="col s6 m4 l4">
+        <div className="col s6 m4 l3">
             <div>
                 <FlippingCard>
                     <FlippingCardBack>
-                        <p><h3>{props.title}</h3></p>
-                        <p style={{fontSize:12}}><h6>{props.overview}</h6></p>
+                        <h3>{props.title}</h3>
+                        <p style={{fontSize:13}}>{props.overview}</p>
                     </FlippingCardBack>
                     <FlippingCardFront>
                         <img src={`http://image.tmdb.org/t/p/w500${props.image}`} style={{width:"100%", height: "100%"}}/>
                     </FlippingCardFront>
+
                 </FlippingCard>
-            </div>
-            <div>
-                <span style={{ contentAlign: "center" }}>
-                    <a href={`/listings/${props.title}`}><Button color='danger'>Book Now</Button></a>{' '}
-                    <a href={`/listings/${props.title}`}><Button color='secondary'>More Info</Button></a>{' '}
-                </span>
+                <div>
+                    <span>
+                        <a href={`/listings/${props.title}`}><Button color='secondary' block>More Info</Button></a>{' '}
+                        <a href={`/listings/${props.title}`}><Button color='danger' block> Book Now</Button></a>{' '}
+                    <br/>
+                    <br/>
+                    </span>
+                </div>
             </div>
         </div>
     )   
