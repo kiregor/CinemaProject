581902385f7dec6ab72e9d153757ca5f0587f292
HamzzAR <hamza_razeq@hotmail.co.uk>
2019-06-18 11:44:30 +0100

    Navbar cleaned up

--- Client/src/components/Header/AppLogin.js
+++ Client/src/components/Header/AppLogin.js
@@ -39,19 +39,18 @@ class AppLogin extends Component {
       <div>
         <Button style={{backgroundColor:'#2A3132'}} onClick={this.toggle}>Log In</Button>
         <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-          <ModalHeader toggle={this.toggle}>Log in</ModalHeader>
+          <ModalHeader toggle={this.toggle}>Log in options</ModalHeader>
           <ModalBody>
             <GoogleLogin
                 clientId="458772893738-hp66otnn9svp5kr1jtg2ivshb398qrte.apps.googleusercontent.com"
-                buttonText="Login"
+                buttonText="Login with Google"
                 onSuccess={responseGoogle}
                 onFailure={responseGoogle}
                 cookiePolicy={'single_host_origin'}
             />
           </ModalBody>
           <ModalFooter>
-            <Button color="primary" onClick={this.toggle}>Log in</Button>{' '}
-            <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+            <Button color="secondary" onClick={this.toggle}>Close</Button>
           </ModalFooter>
         </Modal>
       </div>
